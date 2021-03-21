1e6605dad88e9102764250f90e27fa0f7b8968ee
Nicholas Ayson <60242602+nickayson@users.noreply.github.com>
2021-03-20 21:00:43 -0700

    Fixed Parse errors and some comments

--- shell.c
+++ shell.c
@@ -15,31 +15,31 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-//=================================================================================================
+//===============================================================================================================
 //Locate pipe char and return inside
-int piperhs(char** chararguments){
+//function for the right pipe
+int piperhs(char** char_args)
+{
     int i = 0;
-    while(chararguments[i] != '\0'){
-        if(!strncmp(chararguments[i], "|", 1)) 
-        {   // found pipe
-            return i;                               // new chararguments starting at offset
+    while(char_args[i] != '\0'){
+        if(!strncmp(char_args[i], "|", 1)) 
+        {   
+            return i;                               // new char_args starting at offset
         }
         i++;
     }
     return -1;
-}// end of locate pipe
-//=================================================================================================
-
-//=================================================================================================
+}// end of locate pipe 
+//===============================================================================================================================
 //start of parse function
-char** parse(char* s, char* chararguments[])
+char** parse(char* s, char* char_args[])
 {
     const char breakchars[2] = {' ','\t'};
     char* p;
     int num_args = 0;
 
     p = strtok(s, breakchars);
-    chararguments[0] = p;
+    char_args[0] = p;
 
     char** REDIR = malloc(2 * sizeof(char*));
 
@@ -53,7 +53,7 @@ char** parse(char* s, char* chararguments[])
 
     while(p != NULL)
     {
-        // add p to chararguments
+        // add p to char_args
         p = strtok(NULL, breakchars);
 
         if(p == NULL) 
@@ -78,36 +78,31 @@ char** parse(char* s, char* chararguments[])
         {   // pipe
             REDIR[0] = "p";        
         }
-        chararguments[num_args++] = p;
+        char_args[++num_args] = p;
     }
     return REDIR;
 }   // end of parse function
-//==================================================================================================
-
-//==================================================================================================
-//main function
 
 int main(int argc, const char* argv[])
 {
     char s [BUFSIZ];
-    char last_command[BUFSIZ]; // most-recently used cache
+    char last_command   [BUFSIZ]; // most-recently used cache
     int pipefd[2];      // file descriptor
 
     // clear buffers
     memset(s, 0, BUFSIZ * sizeof(char));
-    memset(last_command, 0, BUFSIZ * sizeof(char));
-   
+    memset(last_command,   0, BUFSIZ * sizeof(char));
+
 
     while(1)
     {
-        printf("osh> ");
+        printf("osh> ");            //given
         fflush(stdout); 
 
         // read into s
         fgets(s, BUFSIZ, stdin);
         s[strlen(s) - 1] = '\0';                // replace newline with null
 
-        // edge cases
         if(strncmp(s, "exit", 4) == 0)          //get out of osh
         {
             return 0;
@@ -118,12 +113,12 @@ int main(int argc, const char* argv[])
         } 
 
         // wait for '&'
-        bool a = true;
+        bool waitx = true;
         char* offset = strstr(s, "&");
         if(offset != NULL)
         {
             *offset = ' '; 
-            a = false;
+            waitx = false;
         }
 
         pid_t pid = fork();
@@ -134,15 +129,15 @@ int main(int argc, const char* argv[])
         }
         else if(pid != 0)
         {   // parent
-            if(a){
+            if(waitx){
                 wait(NULL);         // wait if used with '&'
                 wait(NULL);
             }
         }
         else
         {   // child
-            char* chararguments[BUFSIZ];
-            memset(chararguments, 0, BUFSIZ * sizeof(char));
+            char* char_args[BUFSIZ];
+            memset(char_args, 0, BUFSIZ * sizeof(char));
 
             int history = 0;
             // if we use '!!' we want to read from last_command
@@ -150,7 +145,7 @@ int main(int argc, const char* argv[])
             {
                 history = 1;
             }
-            char** redirect = parse( (history ? last_command : s), chararguments);
+            char** redirect = parse( (history ? last_command : s), char_args);
             // no command entered before history function
             if(history && last_command[0] == '\0')
             {
@@ -158,61 +153,63 @@ int main(int argc, const char* argv[])
                 exit(0);
             } 
             if(!strncmp(redirect[0], "o", 1))
-            {   // output redirect
+            {   // output redirect for the output
                 printf("Output saved to the file./%s\n", redirect[1]);
                 int fd = open(redirect[1], O_TRUNC | O_CREAT | O_RDWR);
                 dup2(fd, STDOUT_FILENO); // redirect stdout to file descriptor 
             }
             else if(!strncmp(redirect[0], "i", 1))
-            {   // s redirect
+            {   // s redirect for the read
                 printf("Reading from file: ./%s\n", redirect[1]);
-                int fd = open(redirect[1], O_RDONLY);
+                int fd = open(redirect[1], O_RDONLY);   
                 memset(s, 0, BUFSIZ * sizeof(char));
                 read(fd, s,  BUFSIZ * sizeof(char));
-                memset(chararguments, 0, BUFSIZ * sizeof(char));
+                memset(char_args, 0, BUFSIZ * sizeof(char));
                 s[strlen(s) - 1]  = '\0';
-                parse(s , chararguments);
+                parse(s , char_args);
             }
             else if(!strncmp(redirect[0], "p", 1))
             {   // found a pipe
                 pid_t pidchild;                 //child
-                int rhsoffset = piperhs(chararguments);
-                chararguments[rhsoffset] = "\0";
+                int rhsoffset = piperhs(char_args);
+                char_args[rhsoffset] = "\0";
                 int i = pipe(pipefd);
+
                 if(i < 0)
                 {
                     fprintf(stderr, "Pipe failed\n");
                     return 1;
                 }
 
-                char *lhs[BUFSIZ], *rhs[BUFSIZ];
+                char* lhs[BUFSIZ], *rhs[BUFSIZ];        //left buffer and right buffer
                 memset(lhs, 0, BUFSIZ*sizeof(char));                //buffers to zero
                 memset(rhs, 0, BUFSIZ*sizeof(char));
 
                 for(int i = 0; i < rhsoffset; i++)
                 {
-                    lhs[i] = chararguments[i];
+                    lhs[i] = char_args[i];
                 }
-                for(int i = 0; i < BUFSIZ; i++){
+                for(int i = 0; i < BUFSIZ; i++)
+                {
                     int ix = ix + rhsoffset + 1;
-                    if(chararguments[i] == 0) 
+                    if(char_args[i] == 0) 
                     {
                         break;
                     }
-                    rhs[i] = chararguments[ix];
+                    rhs[i] = char_args[ix];
                 }
-                
+
                 pidchild = fork();                      // create child to handle pipe's rhs
                 if(pidchild < 0)
                 {
                     fprintf(stderr, "fork failed\n");
                     return 1;
                 }
-                else if(pidchild != 0)
+                if(pidchild != 0)
                 {   // parent process 
                     dup2(pipefd[1], STDOUT_FILENO);         // duplicate stdout to write end of file descriptor
                     close(pipefd[1]);                       // close write
-                    execvp(lhs[0], lhs);
+                    execvp(lhs[0], lhs);                    //execvp is used in child process
                     exit(0); 
                 }
                 else
@@ -224,12 +221,12 @@ int main(int argc, const char* argv[])
                 }
                 wait(NULL);
             }
-            execvp(chararguments[0], chararguments);                                //The things args array will be passed to
+            execvp(char_args[0], char_args);                                //The things args array will be passed to
             exit(0);  
         }
     }
 
     return 0;
 }
-//======================================================================================================================
 //end of main
+//===================================================================================================================
